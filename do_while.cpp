0c725bf320c1a58d515d1a48a06ba08a41de457d
Rodolfo ceo cortes <ceosmarck@gmail.com>
2022-12-31 19:24:19 -0600

    first

---
+++ do_while.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numb;
+    do
+    {
+        cout << "Enter a number ";
+        cin >> numb;
+    } while (numb <= 100);
+    system("PAUSE");
+    return 0;
+}
